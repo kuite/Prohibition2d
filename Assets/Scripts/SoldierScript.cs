e181ff20483323fb81091850ebfbbcd83999f08a
zimi <92szymon@gmail.com>
2017-01-17 20:35:05 +0100

    City scene test started, basic fight scene started

---
+++ Assets/Scripts/SoldierScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoldierScript : MonoBehaviour {
+	private Rigidbody2D rigi;
+
+	// Use this for initialization
+	void Start () {
+		rigi = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Approach (3.0f, 3.0f);
+	}
+
+	private bool Approach(float x, float y)
+	{
+		float approachSpeed = 3;
+		Vector2 approachDestination = new Vector2(x, y);
+		Vector2 myPoss = rigi.position;
+
+		if (Vector2.Distance(approachDestination, myPoss) < 0.2f)
+		{
+			rigi.velocity = new Vector2(0f, 0f);
+			return true;
+		}
+
+		rigi.velocity = new Vector2(approachSpeed * (approachDestination.x - myPoss.x) / Vector2.Distance(approachDestination, myPoss), approachSpeed * (approachDestination.y - myPoss.y) / Vector2.Distance(approachDestination, myPoss));
+		return false;
+	}
+}
