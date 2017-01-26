b5a9e1882ca96d9e72adc57f34563245a8cff374
zimi <92szymon@gmail.com>
2017-01-26 21:08:57 +0100

    Fight scene moved to another folder and A star path finding implemented

--- Assets/Scripts/SoldierScript.cs
+++ Assets/Scripts/SoldierScript.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class SoldierScript : MonoBehaviour {
 	private Rigidbody2D rigi;
-
+	private Stack waypointsStack = new Stack ();
+	public Vector2 destinationVector;
+	private Vector2[] waypoints;
 	// Use this for initialization
 	void Start () {
 		rigi = GetComponent<Rigidbody2D>();
+		destinationVector = transform.position;
+		waypoints = PathFindingScript.RequestPath (new Vector2(0.0f, 0.0f), new Vector2(0.0f, 4.0f));
+		foreach (Vector2 point in waypoints) {
+			waypointsStack.Push (point);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Approach (3.0f, 3.0f);
+		FollowWaypoints ();
 	}
 
-	private bool Approach(float x, float y)
+	private bool Approach(Vector2 wayP)//float x, float y)
 	{
-		float approachSpeed = 3;
-		Vector2 approachDestination = new Vector2(x, y);
+		float approachSpeed = 0.5f;
+		Vector2 approachDestination = new Vector2(wayP.x, wayP.y);
 		Vector2 myPoss = rigi.position;
 
 		if (Vector2.Distance(approachDestination, myPoss) < 0.2f)
@@ -29,5 +36,11 @@ public class SoldierScript : MonoBehaviour {
 
 		rigi.velocity = new Vector2(approachSpeed * (approachDestination.x - myPoss.x) / Vector2.Distance(approachDestination, myPoss), approachSpeed * (approachDestination.y - myPoss.y) / Vector2.Distance(approachDestination, myPoss));
 		return false;
+	}
+
+	void FollowWaypoints(){
+		if (Approach (destinationVector) && waypointsStack.Count > 0) {
+			destinationVector = (Vector2)waypointsStack.Pop ();
+		}
 	}
 }
