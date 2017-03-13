da539fac0e29921c99995216e295e147f971f3d3
kuite <piomost@gmail.com>
2017-03-13 22:09:52 +0100

    missing files

---
+++ Assets/Model/GameSettings/SoldierSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Model.Context;
+
+namespace Assets.Model.GameSettings
+{
+    public class SoldierSettings : IEntity
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+    }
+}
