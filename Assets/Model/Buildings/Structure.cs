b3e8c486570f91c6947ba031fd04ec64bbc2a176
kuite <piomost@gmail.com>
2017-03-22 11:57:10 +0100

    structure

---
+++ Assets/Model/Buildings/Structure.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Assets.Model.Context;
+
+namespace Assets.Model.Buildings
+{
+    public abstract class Structure
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public int Level { get; set; }
+
+        public bool UpgradeLevel()
+        {
+            Level++;
+            return true;
+        }
+
+        public bool DowngradeLevel()
+        {
+            Level--;
+            return true;
+        }
+    }
+}
